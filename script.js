1795c4b219dcd1f49e2849e7226704b0f0f4ad6f
feifei <1026835233@qq,com>
2017-11-17 22:46:05 +0800

    全屏 a

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var Expression = document.getElementById("Expression"),
     result = document.getElementById("result");
 
 function onclickNum(e){
+    if(result.innerHTML !== ""){
+        Expression.innerHTML ="";
+        result.innerHTML = "";
+    }
     Expression.innerHTML += e;
 }
 function onclickClear(e){
@@ -13,5 +17,5 @@ function onclickResult(e){
     result.innerHTML=eval(Expression.textContent);
 }
 function onclickA(){
-    Expression.innerText = '';
+    Expression.innerHTML="温馨提示：小数点和除都不太好用";
 }
